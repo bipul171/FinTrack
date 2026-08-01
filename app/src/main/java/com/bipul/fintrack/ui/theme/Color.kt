cffb201e5b5e41ce525295d711f375b29c1f09a6
Md. Bipul Mia <mdbipulprocoder@gmail.com>
2026-08-02 01:15:49 +0600

    feat: add home dashboard UI

--- app/src/main/java/com/bipul/fintrack/ui/theme/Color.kt
+++ app/src/main/java/com/bipul/fintrack/ui/theme/Color.kt
@@ -9,3 +9,18 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+// My Colors
+
+val FinTrackPrimary = Color(0xFF0F766E)      // Deep Teal
+val FinTrackPrimaryDark = Color(0xFF115E59)  // Dark Teal
+
+val ScreenBackground = Color(0xFFF8FAFC)     // Soft Off-White
+val TextPrimary = Color(0xFF1E293B)          // Dark Slate
+val TextSecondary = Color(0xFF64748B)        // Slate Gray
+
+val FieldBackground = Color(0xFFFFFFFF)      // White
+val BorderColor = Color(0xFFCBD5E1)          // Light Gray
+
+val ErrorColor = Color(0xFFDC2626)           // Professional Red
