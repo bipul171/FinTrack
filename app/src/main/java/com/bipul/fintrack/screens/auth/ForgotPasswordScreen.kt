e6df4cf1e4b5764b073031fd6b6c94fe7a2faab2
Md. Bipul Mia <mdbipulprocoder@gmail.com>
2026-08-20 23:38:31 +0600

    feat: add forgot password screen

--- app/src/main/java/com/bipul/fintrack/screens/auth/ForgotPasswordScreen.kt
+++ app/src/main/java/com/bipul/fintrack/screens/auth/ForgotPasswordScreen.kt
@@ -1,2 +1,157 @@
 package com.bipul.fintrack.screens.auth
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bipul.fintrack.ui.theme.FinTrackPrimary
+import com.bipul.fintrack.ui.theme.ScreenBackground
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ForgotPasswordScreen() {
+
+    Scaffold(
+        contentColor = ScreenBackground
+
+    ) {innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues = innerPadding)
+                .padding(top = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Transparent,
+                        contentColor = Color.Black
+                    )
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Arrow Back"
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Image(
+                painter = painterResource(com.bipul.fintrack.R.drawable.fintrack_logo),
+                contentDescription = "Logo",
+                modifier = Modifier.size(150.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "Forgot Password?",
+                fontSize = 30.sp
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "Enter your email address and we'll send " +
+                        "you a link to reset your password.",
+                fontSize = 16.sp,
+                modifier = Modifier.padding(horizontal = 16.dp),
+                color = Color.Black
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Email,
+                        contentDescription = "Email"
+                    )
+                },
+                label = {
+                    Text(
+                        text = "Email",
+                        fontSize = 16.sp
+                    )
+                },
+                modifier = Modifier.fillMaxWidth()
+                    .padding(all =16.dp)
+                    .height(60.dp),
+                shape = RoundedCornerShape(16.dp)
+            )
+
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+                    .height(56.dp),
+                shape = RoundedCornerShape(16.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = FinTrackPrimary,
+                    contentColor = Color.White
+                )
+            ){
+                Text(
+                    text = "Send Rest Link",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextButton(
+                onClick = {}
+            ) {
+                Text(
+                    text = "Back to Sign In",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = FinTrackPrimary
+                )
+            }
+        }
+
+
+    }
+
+}
